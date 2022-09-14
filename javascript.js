5926df134e75e28fcca7fff8bad6c07708399c07
Serena867 <104524200+Serena867@users.noreply.github.com>
2022-09-14 13:32:42 -0700

    initial commit

---
+++ javascript.js
@@ -0,0 +1,66 @@
+function getComputerChoice(){
+    let result = Math.floor(Math.random() * 3)
+    let resultName = ""
+    switch (result){
+        case 0:
+            resultName = "Rock"
+            break;
+
+        case 1:
+            resultName = "Paper"
+            break;
+
+        case 2:
+            resultName = "Scissors"
+            break;
+    }
+    console.log("Computer choice: " + resultName)
+    return result
+}
+
+function getUserChoice(){
+    let userChoice = parseInt(prompt("Please make a choice: Rock, Paper, or Scissors"))
+    userChoice = userChoice.toLowerCase()
+    let userResult = -1
+    switch (userChoice){
+        case "rock":
+            userResult = 0
+            break;
+        case "paper":
+            userResult = 1
+            break;
+        case "scissors":
+            userResult = 2
+            break;
+        default:
+            alert("Invalid choice or incorrect spelling")
+            getUserChoice()
+            break;
+    }
+    console.log("Your choice: " + userChoice)
+    return result;
+}
+
+function compareChoices(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice){
+        console.log("This round is a tie")
+    } else if (playerChoice === 0){
+        if (computerChoice === 1){
+            console.log("The computer wins this round")
+        } else {
+            console.log("You win this round")
+        }
+    } else if (playerChoice === 1){
+        if (computerChoice === 2) {
+            console.log("The computer wins this round")
+        } else {
+            console.log("You win this round")
+        }
+    } else {
+        if (computerChoice === 0){
+            console.log("The computer wins this round")
+        } else {
+            console.log("You win this round")
+        }
+    }
+}
